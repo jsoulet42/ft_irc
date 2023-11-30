1b15e91e0d12914920ea1aa5a709a1e54d9b0af7
jsoulet42 <illidan888@hotmail.fr>
2023-11-30 01:30:28 +0100

    protocole connexion down + refactoring

--- ft_irc.hpp
+++ ft_irc.hpp
@@ -13,4 +13,15 @@
 #include <sstream>
 
 #define BUFFSIZE 1024
-#define IPHOST '127.0.0.1'
+#define IPHOST ":127.0.0.1"
+#define ERRORP421 " 421 PASS :Unknown command\r\n"
+#define ERRORP461 " 461 PASS :Not enough parameters\r\n"
+#define ERRORP464 " 464 PASS :Password incorrect\r\n"
+#define ERRORC421 " 421 CAP :Unknown command\r\n"
+#define ERRORN421 " 421 NICK :Unknown command\r\n"
+#define ERRORN432 " 432 NICK :Erroneus nickname\r\n"
+#define ERRORN433 " 433 NICK :Nickname is already in use\r\n"
+#define ERRORN431 " 431 NICK :No nickname given\r\n"
+#define ERRORU421 " 421 USER :Unknown command\r\n"
+#define ERRORU461 " 461 USER :Not enough parameters\r\n"
+
